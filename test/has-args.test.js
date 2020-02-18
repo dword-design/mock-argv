8db7a2c107cefe378fad048830e67aa7b95df3ee
dword <sebastianlandw@gmail.com>
2020-02-18 07:43:43 +0000

    fix: fixes after upgrades

--- test/has-args.test.js
+++
@@ -1,6 +0,0 @@
-import mockArgv from 'mock-argv'
-import firstArgv from './first-argv'
-
-export default done => {
-  mockArgv(['foo', 'bar'], () => { expect(process.argv).toEqual([...firstArgv, 'foo', 'bar']); done() })
-}
--- test/missing-args.test.js
+++
@@ -1,6 +0,0 @@
-import mockArgv from 'mock-argv'
-import firstArgv from './first-argv'
-
-export default done => {
-  mockArgv(() => { expect(process.argv).toEqual(firstArgv); done() })
-}
